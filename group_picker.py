1584feaace39db45a0935d326e149a6628c7720c
Ben <benranson5@gmail.com>
2021-04-19 12:00:46 +0100

    added overflow

--- group_picker.py
+++ group_picker.py
@@ -24,17 +24,26 @@ def split_list(mem_count):
     print("\n")
 
     for _ in range(0, len(members), 1):
+
         rand = random.randint(0, len(members) - 1)
         string += members[rand]
         members.pop(rand)
         count += 1
         if count == mem_count:
+            if len(members) < mem_count:
+                string += ", " + members[0]
+                members.pop(0)
             print(f"Team {team_no}: {string}")
+
             string = ""
             count = 0
             team_no += 1
+
         else:
             string += ", "
+
+        if len(members) == 0:
+            break
     # If teams are uneven in numbers then print the uneven team
     if string:
         print(f"Team {team_no}: {string[:-2]}")
